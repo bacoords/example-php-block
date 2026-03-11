204b6f20064770c7c7f6ac8cd34cdb2ba9cc4ff1
Brian Coords <bacoords@gmail.com>
2026-03-11 15:31:37 -0700

    fix formatting errors

--- example-php-block.php
+++ example-php-block.php
@@ -5,6 +5,8 @@
  * Version: 1.0.0
  * Requires at least: 7.0
  * Author: Developer
+ *
+ * @package example-php-block
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -16,7 +18,10 @@ define( 'EPB_URL', plugin_dir_url( __FILE__ ) );
 // Load block registrations.
 require_once EPB_DIR . 'basic-block/register.php';
 
-// Check if WooCommerce is active.
+/**
+ * Example of hooking into a plugin-specific action to register blocks conditionally.
+ * This ensures that the WooCommerce block is only registered if WooCommerce is active.
+ */
 function epb_woocommerce_loaded() {
 	require_once EPB_DIR . 'woo-block/register.php';
 	// Custom code here. WooCommerce is active and all plugins have been loaded...
